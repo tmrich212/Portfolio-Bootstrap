2141ccbf6c3bd407210c1ea3379ac745a4267c20
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2021-10-14 21:46:22 -0400

    js animation added to homepage

--- Pages/Pages/app.js
+++ Pages/Pages/app.js
@@ -1,69 +1,39 @@
-const carouselText = [
-  {text: 'Website'},
-  {text: 'Content Manager'},
-  {text: 'Friend'}
-];
+let typeText = document.querySelector('.typeText');
+let textToType = "Self-taught Front-end Developer";
+let index = 0, isAdding = true;
 
-$(document).ready(async function() {
-  carouselText(carouselText, "#feature-text");
-})
-
-async function typeSentence(sentence, eleRef, delay = 100) {
-  const letters = sentence.split("");
-
-  let i = 0;
-  while(i < letters.length) {
-    await waitForMs(delay);
-    $(eleRef).append(letters[i]);
-    i++
-  }
-  return;
-}
-
-function waitForMs(delay) {
-  return new Promise(resolve => setTimeOut(resolve, ms));
-}
-
-async function deleteSentence(eleRef){
-  const sentence = $(eleRef).html();
-  const letters = sentence.split("");
-  
-  let i = 0;
-  while(letters.length > 0){
-    await waitForMs(100);
-    letters.pop();
-    $(eleRef).html(letters.join(''));
-  }
-}
-
-async function carousel(carouselList, eleRef) {
-  let i = 0;
-
-  while(true){
-    updateFontColor(eleRef, carouselList[i].color);
-    await typeSentence(carouselList[i].text, eleRef);
-    await waitForMs(1500);
-    await deleteSentence(eleRef);
-    await waitForMs(500);
-
-    i++;
-    if(i >= carouselList.length) {
-      i=0;
+function playAnim() {
+  setTimeout(function (){
+    // set text of typeText to textToType using index
+    typeText.innerText = textToType.slice(0, index)
+    if (isAdding) {
+      // adds text
+      if(index > textToType.length){
+        // stops adding text
+        isAdding = false;
+        // break: wait 2s before playing again
+        setTimeout( function () {
+          playAnim()
+        }, 5000)
+        return;
+      } else {
+        // increment index by 1
+        index++;
+      }
+    } else {
+      // remove text
+      if(index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--;
+      }
     }
-  }
+    // call function to run
+    playAnim();
+  }, 120)
 }
 
-function updateFontColor(eleRef, color){
-  $(eleRef).css('color', color);
-}
-
-function waitForMs(ms){
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-
-$( document ).ready(async function() {
-  await typeSentence("Need a website?", "#sentence");
-  await waitForMs(2000);
-  deleteSentence("#sentence")
-});
+//start animation
+playAnim();
